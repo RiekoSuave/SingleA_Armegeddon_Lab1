19bb28da8cca1bdde12bedf10cb5ed9c6e0b0271
jareonbailey-web <jareon.bailey@gmail.com>
2026-05-21 21:32:07 +0400

    first commit

--- app.py
+++ app.py
@@ -5,12 +5,22 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
 
 while True:
     print("\n=== Calculator Menu ===")
     print("1. Add")
     print("2. Subtract")
-    print("3. Exit")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
 
     choice = input("Choose an option: ")
 
@@ -31,8 +41,22 @@ while True:
         print("Answer:", result)
 
     elif choice == "3":
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        print("Answer:", multiply(num1, num2))
+
+    elif choice == "4":
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        print("Answer:", divide(num1, num2))
+    
+    
+    
+    elif choice == "5":
         print("Goodbye!")
         break
 
     else:
         print("Invalid option.")
+
+     
